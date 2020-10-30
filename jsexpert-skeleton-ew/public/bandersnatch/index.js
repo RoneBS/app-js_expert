86437f83a489061e985c02194060e47f98f940d4
Rone Silveira <ronebs10@hotmail.com>
2020-10-30 15:21:56 -0300

    finalizing app, adding access to videos by modal

--- jsexpert-skeleton-ew/public/bandersnatch/index.js
+++ jsexpert-skeleton-ew/public/bandersnatch/index.js
@@ -2,20 +2,22 @@ const MANIFEST_URL = 'manifest.json'
 const localHost = ['127.0.0.1', 'localhost']
 
 async function main() {
-    const isLocal = !!~localHost.indexOf(window.location.hostname)
+    const isLocal = !!~localHost.indexOf(window.location.hostname)  
     console.log('isLocal?', isLocal)
     const manifestJSON = await (await fetch(MANIFEST_URL)).json()
     const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
     const videoComponent = new VideoComponent()
     const network = new Network({ host })
-    const videoPayer = new VideoMediaPlayer({
+    const videoPlayer = new VideoMediaPlayer({
         manifestJSON,
-        network
+        network,
+        videoComponent
     })
 
-    videoPayer.initializeCodec()
+    videoPlayer.initializeCodec()
     videoComponent.initializePlayer()
 
+    window.nextChunk = (data) => videoPlayer.nextChunk(data)
 }
 
 window.onload = main
